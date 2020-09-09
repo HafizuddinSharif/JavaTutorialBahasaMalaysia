f8fb8546bda673af30517528654f58e1eba82667
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2020-09-09 12:32:52 +0100

    Initial commit

---
+++ src/PrintAndComment.java
@@ -0,0 +1,17 @@
+public class PrintAndComment {
+
+    public static void main(String[] args) {
+
+//        Hello hari ini adalah hari yang baik
+
+        /* Hello
+        Hai
+         */
+
+        System.out.println("Sharif hensem");
+
+        System.out.println("Mak saya baik");
+
+    }
+
+}
