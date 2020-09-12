d5c5e8b8501003310488d93aaf4f2a6966ce8453
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2020-09-12 15:09:33 +0100

    Add DataTypes.java file

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,2 +1,32 @@
 public class DataTypes {
+
+    public static void main(String[] args) {
+
+//        int myInteger;
+//
+//        myInteger = 10;
+
+//        double myDouble = 23.5;
+
+//        char myChar, myChar1, myCHar2;
+//
+//        myChar = 'a';
+//        myChar1 = 'b';
+//        myCHar2 = 'A';
+
+//        boolean myBool = true;
+//        boolean myBool1 = false;
+
+        String myString = "Aliah";
+
+        System.out.println(myString);
+        System.out.println(myString);
+        System.out.println(myString);
+
+        myString = "Sharif";
+
+        System.out.println(myString);
+        System.out.println(myString);
+        System.out.println(myString);
+    }
 }
